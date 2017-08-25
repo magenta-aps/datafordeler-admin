738a0026ea16d9f0a592855c9e78cfc06c786443
AlexanderMomtaz <alexander@magenta.dk>
2017-08-25 08:05:00 +0200

    Added titles for pages and moved most of javascript inside dafo.js Also move dafo-styles.css inside main.css (now style.css)

--- django/dafousers/static/resources/scripts/dafo.js
+++ django/dafousers/static/resources/scripts/dafo.js
@@ -1,11 +1,30 @@
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 var Password = {
 
     _pattern : /[a-zA-Z0-9]/,
 
-
-    _getRandomByte : function()
-    {
-        // http://caniuse.com/#feat=getrandomvalues
+    _getRandomByte : function() {
         if(window.crypto && window.crypto.getRandomValues)
         {
             var result = new Uint8Array(1);
@@ -24,8 +43,7 @@ var Password = {
         }
     },
 
-    generate : function(length)
-    {
+    generate : function(length) {
         return Array.apply(null, {'length': length})
             .map(function()
             {
@@ -40,6 +58,13 @@ var Password = {
                 }
             }, this)
             .join('');
+    },
+
+    new_password : function() {
+        var new_pw = this.generate(16);
+        document.getElementById("id_password").value = new_pw;
+        document.getElementById("password_display").innerHTML = new_pw;
     }
+
 
 };
