22fbb2558b9a223d4ab4a834286bbe9e41d5c4b2
Lars Peter Thomsen <larsp@magenta.dk>
2018-01-17 14:33:20 +0100

    Log users logging in and out

--- django/common/apps.py
+++ django/common/apps.py
@@ -5,3 +5,10 @@ from django.apps import AppConfig
 
 class CommonConfig(AppConfig):
     name = 'common'
+
+    signals_loaded = False
+
+    def ready(self):
+        if not self.signals_loaded:
+            import common.signals  # noqa
+            self.signals_loaded = True
