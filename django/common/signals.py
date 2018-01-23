572ef2673dbe6b9192233bd1402680f7a902b765
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2018-01-23 12:55:57 +0100

    Add **kwargs to signal methods

--- django/common/signals.py
+++ django/common/signals.py
@@ -6,13 +6,14 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 
 @receiver(user_logged_in)
-def on_login(sender, signal, request, user):
+def on_login(sender, signal, request, user, **kwargs):
     logging.getLogger('django.server').info(
         "User %s logged in", unicode(user)
     )
 
+
 @receiver(user_logged_out)
-def on_logout(sender, signal, request, user):
+def on_logout(sender, signal, request, user, **kwargs):
     logging.getLogger('django.server').info(
         "User %s logged out", unicode(user)
     )
