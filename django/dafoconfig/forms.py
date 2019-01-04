07e26b33abdce0b78aa4ce47891781de7fb35b20
Lars Peter Thomsen <larsp@magenta.dk>
2019-01-04 08:41:32 +0100

    Hide password data

--- django/dafoconfig/forms.py
+++ django/dafoconfig/forms.py
@@ -6,7 +6,49 @@ from .models import CvrConfig, CprConfig, GladdregConfig
 
 
 class ConfigurationForm(ModelForm):
-    pass
+
+    password_placeholder = '********'
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user")
+        super(ConfigurationForm, self).__init__(*args, **kwargs)
+        for field_name in self.initial:
+            if 'password' in field_name:
+                self.initial[field_name] = self.password_placeholder
+
+    def _cronfield_changed(self):
+        return any(isinstance(self.instance._meta.get_field(field_name),
+                              fancy_cronfield.fields.CronField)
+                   for field_name in self.changed_data)
+
+    def clean(self):
+        cleaned_data = super(ConfigurationForm, self).clean()
+        real_clean = {}
+        for name, data in cleaned_data:
+            if 'password' in name:
+                if data == self.password_placeholder:
+                    continue
+            real_clean[name] = data
+        return real_clean
+
+    def save(self, commit=True):
+        instance = super(ConfigurationForm, self).save(commit)
+
+        if self._cronfield_changed():
+            Command(
+                commandname="schedule-changed",
+                commandbody=json.dumps(
+                    {
+                        'table': self.instance._meta.db_table,
+                        'fields': self.changed_data,
+                        'id': self.instance.id,
+                    }),
+                issuer="admin interface",
+                received=datetime.datetime.now(),
+                status=Command.STATUS_QUEUED,
+            ).save()
+
+        return instance
 
 
 class CvrConfigurationForm(ConfigurationForm):
