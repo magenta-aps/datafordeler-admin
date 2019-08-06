63448fba936bcd2fa7f4250ad3166d5024725764
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2019-08-06 13:39:19 +0200

    more tests for the common module

--- django/common/tests/test_views.py
+++ django/common/tests/test_views.py
@@ -24,25 +24,34 @@ class ViewsTestCase(TestCase):
         self.client.logout()
 
     def test_frontpage_not_logged_in(self):
-        resp = self.client.get('/frontpage/')
+        resp = self.client.get(reverse('common:frontpage'))
         self.assertEqual(resp.status_code, 302)
 
     def test_frontpage(self):
         self.client.login(**{'username': 'jakob@data.nanoq.gl', 'password': 'jacob'})
-        resp = self.client.get('/frontpage/')
+        resp = self.client.get(reverse('common:frontpage'))
         self.assertEqual(resp.status_code, 200)
         self.client.logout()
 
     def test_login_user_password(self):
-        resp = self.client.post('/login/', {'username': 'jakob@data.nanoq.gl', 'password': 'jacob'}, follow=True)
+        resp = self.client.post(reverse('common:login'), {'username': 'jakob@data.nanoq.gl', 'password': 'jacob'}, follow=True)
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(resp.context['user'].is_authenticated())
 
     def test_login_user_password_fail(self):
-        resp = self.client.post('/login/', {'username': 'jako@data.nanoq.gl', 'password': 'jacob'}, follow=True)
+        resp = self.client.post(reverse('common:login'), {'username': 'jako@data.nanoq.gl', 'password': 'jacob'}, follow=True)
         self.assertEqual(resp.status_code, 200)
         self.assertFalse(resp.context['user'].is_authenticated())
 
+    def test_logout(self):
+        self.client.login(**{'username': 'jakob@data.nanoq.gl', 'password': 'jacob'})
+        resp = self.client.post(reverse('common:logout'), follow=True)
+        self.assertFalse(resp.context['user'].is_authenticated())
+
+    def test_js_catalog(self):
+        resp = self.client.get(reverse('common:javascript-catalog'))
+        self.assertEqual(resp.status_code, 200)
+
     def test_rst_doc_view(self):
-        resp = self.client.get('/doc/access-rights.rst')
+        resp = self.client.get(reverse('common:doc', args=['access-rights']))
         self.assertEqual(resp.status_code, 200)
